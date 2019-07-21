65d21dc807f036128b95ff7ae9cc470253302148
ms11786 <ms11786@gmail.com>
2019-07-21 13:05:35 -0600

    more work on pathfinding of enemy waves

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startingWave = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int x = 0; x < waveConfig.GetNumberOfEnemies(); x++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
