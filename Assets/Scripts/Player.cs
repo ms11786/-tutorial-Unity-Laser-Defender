ad7d2fb96e2422885de42b579839579c1328af59
ms11786 <ms11786@gmail.com>
2019-07-20 22:39:00 -0600

    clamp movement; add padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -21,12 +36,12 @@ public class Player : MonoBehaviour
     private void Move()
     {
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float newXPos = transform.position.x + deltaX;
+        float newXPos = Mathf.Clamp((transform.position.x + deltaX), xMin, xMax);
 
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        float newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, newYPos);
+        float newYPos = Mathf.Clamp((transform.position.y + deltaY), yMin, yMax);
 
+        transform.position = new Vector2(newXPos, newYPos);
         
     }
 }
