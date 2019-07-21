db33d328f1cdddfcf17de28c19d6029f067773f1
ms11786 <ms11786@gmail.com>
2019-07-21 16:50:43 -0600

    allow enemies to take damage; new enemy waves and enemy type created;

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
-    [SerializeField] float moveSpeed = 2f;
-
+    
     List<Transform> waypoints;
-
+    WaveConfig waveConfig;
     int waypointIndex = 0;
 
     private void Start()
@@ -22,12 +20,18 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move()
     {
+        
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
 
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
             if (transform.position == targetPosition)
